0d308ff474ac3993775882159c2d5319ef6b1191
Doug Wegscheid <dwegscheid@sbcglobal.net>
2022-01-09 23:08:15 -0500

    git properties is not always picked up by processResources.
    Needs work.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,7 +41,8 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     logger = EventLogging.getLogger(Robot.class, Level.INFO);
-    logger.info ("I'm alive! {}", GitNess.gitString());
+    logger.info ("I'm alive! {}", GitNess.gitDescription());
+    logger.info ("the long version: {}", GitNess.gitString());
 
     PortForwarder.add (10080, "frcvision.local", 80);
     PortForwarder.add (10022, "frcvision.local", 22);
--- src/main/java/org/usfirst/frc3620/misc/GitNess.java
+++ src/main/java/org/usfirst/frc3620/misc/GitNess.java
@@ -9,27 +9,77 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
-import org.ejml.generic.GenericMatrixOps_F32;
 import org.slf4j.Logger;
 import org.usfirst.frc3620.logger.EventLogging;
 import org.usfirst.frc3620.logger.EventLogging.Level;
 
 import edu.wpi.first.wpilibj.Filesystem;
 
-/** Add your docs here. */
 public class GitNess {
     static Logger logger = EventLogging.getLogger(GitNess.class, Level.INFO);
+    private static Properties instance;
     public static Properties gitProperties() {
-        Properties gitProperties = new Properties();
-        File propFile = new File(Filesystem.getDeployDirectory(), "git.properties");
+        if (instance == null) {
+            instance = new Properties();
+            File propFile = new File(Filesystem.getDeployDirectory(), "git.properties");
 
-        // .close of resourceStream automagically happens
-        try (InputStream resourceStream = new FileInputStream(propFile)) {
-            gitProperties.load(resourceStream);
-        } catch (Exception ex) {
-            gitProperties.setProperty("oopsie", ex.toString());
+            // .close of resourceStream automagically happens
+            try (InputStream resourceStream = new FileInputStream(propFile)) {
+                instance.load(resourceStream);
+            } catch (Exception ex) {
+                instance.setProperty("oopsie", ex.toString());
+            }
         }
-        return gitProperties;
+        return instance;
+    }
+
+    public static String gitDescription() {
+        Properties p = gitProperties();
+        StringBuilder sb = new StringBuilder();
+        String d;
+        
+        d = p.getProperty("project.dir");
+        if (d == null) {
+            d = "unknown-project";
+        }
+        sb.append(d);
+        sb.append(" ");
+
+        d = p.getProperty("git.commit.id.describe");
+        if (d == null) {
+            d = "unknown";
+        }
+        sb.append(d);
+
+        d = p.getProperty("git.dirty");
+        if (d == null) {
+            d = "-unknownDirtyness";
+        } else {
+            d = Boolean.parseBoolean(d) ? "-dirty" : "";
+        }
+        sb.append(d);
+
+        d = p.getProperty("git.branch");
+        if (d == null) {
+            d = " Branch=null";
+        } else {
+            d = " Branch=\"" + d + "\"";
+        }
+        sb.append(d);
+
+        d = p.getProperty("build.time");
+        if (d != null) {
+            d = " Built time \"" + d + "\"";
+        }
+        sb.append(d);
+
+        d = p.getProperty("git.build.host");
+        if (d != null) {
+            d = " Build host \"" + d + "\"";
+        }
+        sb.append(d);
+
+        return sb.toString();
     }
 
     public static String gitString() {
