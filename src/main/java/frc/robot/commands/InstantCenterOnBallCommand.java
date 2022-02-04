9b3d76354c02647e01aa6aa5647b7a56f84fd57b
GYuan55 <garselovescake@gmail.com>
2022-02-03 21:18:06 -0500

    new CenterOnBallCommand instand command, doesn't work yet

--- src/main/java/frc/robot/commands/InstantCenterOnBallCommand.java
+++ src/main/java/frc/robot/commands/InstantCenterOnBallCommand.java
@@ -4,17 +4,16 @@
 
 package frc.robot.commands;
 
-/*import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance; */
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.miscellaneous.DriveVectors;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.VisionSubSystem;
 
-public class CenterOnBallCommand extends CommandBase {
-  /** Creates a new CenterOnBallCommand. */
-  
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class InstantCenterOnBallCommand extends InstantCommand {
   private DriveSubsystem driveSubsystem;
   private VisionSubSystem visionSubsystem;
 
@@ -23,18 +22,18 @@ public class CenterOnBallCommand extends CommandBase {
   static double MIN_ROTATE_SPEED=0.3;
 
   double targetX = 0;
+  double startNavX = 0;
   //Set Up Network Tables
   //private NetworkTableInstance inst = NetworkTableInstance.getDefault();
   //private NetworkTable networkTable = inst.getTable("V/Target");
   //private NetworkTableEntry targetX = networkTable.getEntry("target.x");
 
   
-  public CenterOnBallCommand(DriveSubsystem driveSubsystem, VisionSubSystem visionSubsystem) {
+  public InstantCenterOnBallCommand(DriveSubsystem driveSubsystem, VisionSubSystem visionSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.driveSubsystem = driveSubsystem;
     this.visionSubsystem = visionSubsystem;
     SmartDashboard.putBoolean("CenterOnBall.running", false);
-
   }
 
   // Called when the command is initially scheduled.
@@ -44,16 +43,12 @@ public class CenterOnBallCommand extends CommandBase {
     driveSubsystem.setForcedManualModeTrue();
     SmartDashboard.putBoolean("CenterOnBall.running", true);
 
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-
     double spinX = 0;
     double spinDegrees = 0;
+    double spinHeading = 0;
     
     targetX = visionSubsystem.getBallXLocation();
+    startNavX = driveSubsystem.getNavXFixedAngle();
 
     if(targetX < 0){
       // Vision doesn't see a ball - set spin to MAX_ROTATE_SPEED
@@ -67,9 +62,12 @@ public class CenterOnBallCommand extends CommandBase {
       spinX = MIN_ROTATE_SPEED + (MAX_ROTATE_SPEED - MIN_ROTATE_SPEED)*(
         (Math.abs(targetX - 0.5))*2);
 
+      spinHeading = startNavX + spinDegrees;
+
       if(targetX<0.5){
           spinX=-spinX;
           spinDegrees=-spinDegrees;
+          spinHeading = startNavX - spinDegrees;
         }     
     }
 
@@ -77,31 +75,6 @@ public class CenterOnBallCommand extends CommandBase {
     SmartDashboard.putNumber("CenterOnBall.spinSpeed",spinX);
     SmartDashboard.putNumber("CenterOnBall.spinDegrees", spinDegrees);
 
-    driveSubsystem.autoDrive(spinDegrees, 0, spinX);
-    //driveSubsystem.twoWheelRotation(spinX);
-
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    SmartDashboard.putBoolean("CenterOnBall.running", false);
-
-    driveSubsystem.teleOpDrive(0,0,0);
-    driveSubsystem.setForcedManualModeFalse();
-
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-
-
-    // This method should return true when the the x-value from the network table reads 0.5 +/- TOLERANCE
-    if (Math.abs(targetX-0.5) <= BALL_LOCATION_TOLERANCE){
-      return true;
-    }
-
-    return false;
+    driveSubsystem.autoDrive(spinHeading, 0, spinX);
   }
 }
