2c1611d8330638c3a1efb0eb0e1ebcf9383e9ca4
drew Kelleher <Drewkelleher6@gmail.com>
2022-02-05 16:01:44 -0500

    most recent working 2/5/22

--- src/main/java/frc/robot/miscellaneous/HomeEncoders.java
+++ src/main/java/frc/robot/miscellaneous/HomeEncoders.java
@@ -11,8 +11,8 @@ public class HomeEncoders {
 	
 	// readings of the absolute encoders when the wheels are pointed at true 0 degrees (gears to front of robot)
 	// PRACTICE BOT!!!!!!!!!
-	public static double PRACTICE_RIGHT_FRONT_ABSOLUTE_OFFSET = -172.2;
-	public static double PRACTICE_LEFT_FRONT_ABSOLUTE_OFFSET = 73.4;
-	public static double PRACTICE_LEFT_BACK_ABSOLUTE_OFFSET = 34.2;
-	public static double PRACTICE_RIGHT_BACK_ABSOLUTE_OFFSET = 58.9;
+	public static double PRACTICE_RIGHT_FRONT_ABSOLUTE_OFFSET = -173.67;
+	public static double PRACTICE_LEFT_FRONT_ABSOLUTE_OFFSET = 73.65;
+	public static double PRACTICE_LEFT_BACK_ABSOLUTE_OFFSET = -29.79;
+	public static double PRACTICE_RIGHT_BACK_ABSOLUTE_OFFSET = 62.05;
 }
