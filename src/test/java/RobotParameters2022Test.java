88d92e1d17a9bcb1e17b00e46da45ed7228fe726
Doug Wegscheid <dwegscheid@sbcglobal.net>
2022-02-17 21:21:43 -0500

    Fix test case. How did this build earlier?!

--- src/test/java/RobotParameters2022Test.java
+++ src/test/java/RobotParameters2022Test.java
@@ -1,3 +1,6 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import frc.robot.RobotParameters2022;
@@ -8,17 +11,15 @@ public class RobotParameters2022Test {
 
     static Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
 
-    static String filename = "src/main/deploy/robot_parameters.json";
+    static Path path = Paths.get("src/main/deploy/robot_parameters.json");
 
     @Test
     public void l0_testBasic() {
-        RobotParameters2022 rb = RobotParametersContainer.getRobotParameters(RobotParameters2022.class, filename, "00-80-2F-18-5C-5F");
+        RobotParameters2022 rb = RobotParametersContainer.getRobotParameters(RobotParameters2022.class, path, "00-80-2F-18-5C-5F");
         System.out.println (rb);
-        rb = RobotParametersContainer.getRobotParameters(RobotParameters2022.class, filename, "00:11:22:33:44:55");
+        rb = RobotParametersContainer.getRobotParameters(RobotParameters2022.class, path, "00:11:22:33:44:55");
         System.out.println (rb);
-        rb = RobotParametersContainer.getRobotParameters(RobotParameters2022.class, filename, "foo");
-        System.out.println (rb);
-        rb = RobotParametersContainer.getRobotParameters(RobotParameters2022.class, null, "foo");
+        rb = RobotParametersContainer.getRobotParameters(RobotParameters2022.class, path, "foo");
         System.out.println (rb);
     }
 }
