de7523809180fb5efeea436fc81f235c0a5e3611
Greg Fisbeck <greg.fisbeck@tempus.com>
2022-02-23 13:51:03 -0600

    Created Initial ShooterCalculator function

--- src/main/java/frc/robot/miscellaneous/ShooterCalculator.java
+++ src/main/java/frc/robot/miscellaneous/ShooterCalculator.java
@@ -7,7 +7,36 @@ package frc.robot.miscellaneous;
 /** Add your docs here. */
 public class ShooterCalculator {
 
+    private static double mainShooterDiameter = 4.0;
+    private static double backspinShooterDiameter = 3.0;
+    private static double desiredCargoRPM = 120.0;
+
     public static double calculateZero (double a) {
         return a-a;
     }
+
+    public static double calculateBackspinRPM (double mainShooterRPM) {
+
+        /* This method should take as input the current RPM of the main shooter
+        *  wheel as well as the CONSTANTS for shooter diameters and desired
+        *  cargo RPM. The method will return the _desired_ RPM of the backspin
+        *  motor. 
+        *
+        *  Please Note: This method assumes that RPM is the correct metric to use.
+        *  If we need something other than RPM (like motor power) we'll need to
+        *  update the calculation to tranlate this to another measure.
+        * 
+        */
+
+        double desiredBackspinRPM;
+
+        desiredBackspinRPM = (mainShooterRPM - (2 * 2.5 * desiredCargoRPM)) * (mainShooterRPM/backspinShooterDiameter);
+
+        return desiredBackspinRPM;
+
+    }
+
+
+
+
 }
--- src/test/java/ShooterCalculatorTest.java
+++ src/test/java/ShooterCalculatorTest.java
@@ -12,4 +12,15 @@ public class ShooterCalculatorTest {
         double calculated = ShooterCalculator.calculateZero(22);
         assertEquals("did not get the expected zero!", expected, calculated, allowedSlop);
     }
+
+
+    @Test
+    public void check1800RPM() {
+
+        double allowdSlop = 0.5;
+        double expected = 1600.0;
+        double calcuated = ShooterCalculator.calculateBackspinRPM(1800.0);
+        assertEquals("did not get the expected result!", expected, calcuated, allowdSlop);
+
+    }
 }
