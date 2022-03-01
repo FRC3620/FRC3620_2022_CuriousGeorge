d4b6b1642aeeeee2c6fa66ddd4944f1ce578c822
Liam Allen <bttffan121@gmail.com>
2022-02-28 20:09:46 -0500

    added multiple speeds to end of preshooterautofire

--- src/main/java/frc/robot/commands/PreshooterAutoFireCommand.java
+++ src/main/java/frc/robot/commands/PreshooterAutoFireCommand.java
@@ -1,30 +1,22 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+// the WPILib BSD license file in the root directory of this projec
 package frc.robot.commands;
-
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.PreShooterSubsystem;
-
-
 public class PreshooterAutoFireCommand extends CommandBase {
   /** Creates a new PreshooterAutoFireCommand. */
   PreShooterSubsystem preShooterSubsystem = RobotContainer.preShooterSubsystem;
   IntakeSubsystem intakeSubsystem = RobotContainer.intakeSubsystem;
-
   boolean intakeRunning = false;
   Timer preshooterTimer = new Timer();
-
-
   public PreshooterAutoFireCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(intakeSubsystem, preShooterSubsystem);
   }
-
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
@@ -34,31 +26,28 @@ public class PreshooterAutoFireCommand extends CommandBase {
       intakeRunning = true;
     }
   }
-
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     intakeSubsystem.spinIntakeBelt(0.0);
     spinPreShooter();
   }
-
-
   public void spinPreShooter(){
     preShooterSubsystem.preshooterOn(1.0);
-
   }
-
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     preShooterSubsystem.preshooterOff();
-    intakeSubsystem.spinIntakeBelt(0.4);
+    if(intakeRunning = true) {
+      intakeSubsystem.spinIntakeBelt(0.4);
+    } else {
+      intakeSubsystem.spinIntakeBelt(0.0);
+    }
   }
-
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-
     if(preshooterTimer.get() > 1){
       return true;
     }
