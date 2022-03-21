646caf567cbb8e0243a682389e8c8a14d57b241e
Drew Kelleher <DrewKelleher6@gmail.com>
2022-03-21 17:33:26 -0400

    working second half of 5 ball auto

--- src/main/java/frc/robot/commands/ShootFarAuto.java
+++ src/main/java/frc/robot/commands/ShootFarAuto.java
@@ -4,6 +4,7 @@ import org.usfirst.frc3620.logger.EventLogging;
 import org.usfirst.frc3620.logger.EventLogging.Level;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -17,16 +18,19 @@ public class ShootFarAuto extends SequentialCommandGroup {
             new StartShooterDataLoggingCommand(getClass().getSimpleName(), 20.0),
             new MoveTurretCommand(turretSubsystem, 180), 
             new LogCommand("done with 180"),
-            new SequentialCommandGroup(
-                new AutoShootCommand(),
-                new LogCommand("found target"),
-                new PullTheTriggerForOneCommand(),
-                new LogCommand("shot once"),
-                new AutoPushBallUpCommand(),
-                new LogCommand("ball is ready to shoot"),
-                new PullTheTriggerForOneCommand(),
-                new AutoPushBallUpCommand(),
-                new PullTheTriggerForOneCommand()
+            new AutoShootCommand(),
+            new LogCommand("found target"),
+            new ParallelCommandGroup(
+                new IntakeOnCommand(),
+                new SequentialCommandGroup(
+                    new PullTheTriggerForOneCommand(),
+                    new LogCommand("shot once"),
+                    new AutoPushBallUpCommand(),
+                    new LogCommand("ball is ready to shoot"),
+                    new PullTheTriggerForOneCommand(),
+                    new AutoPushBallUpCommand(),
+                    new PullTheTriggerCommand()
+                )
             )
         );
  
