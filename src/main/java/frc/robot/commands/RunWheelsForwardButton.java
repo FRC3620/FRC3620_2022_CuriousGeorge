466d14de8188e0e54e45c3e8c74e10017e92a24d
Liam Allen <15163@stjoebears.com>
2022-07-26 19:14:15 -0400

    changes made 7/21/22 (I didn't make them)

--- src/main/java/frc/robot/commands/RunWheelsForwardButton.java
+++ src/main/java/frc/robot/commands/RunWheelsForwardButton.java
@@ -6,12 +6,37 @@ package frc.robot.commands;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class RunWheelsForwardButton extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final DriveSubsystem driveSubsystem;
+
+  public boolean areTheyClose(double a, double b){
+
+    double larger = Double.max(a, b);
+
+    double absoluteValue = Math.abs(a-b);
+  
+    if (absoluteValue/larger>0.1){return false;}
+	return true;
+  }
+  public boolean areAllwheelsok(){
+    double lf = driveSubsystem.leftFrontDriveEncoder.getVelocity();
+    double lb = driveSubsystem.leftBackDriveEncoder.getVelocity();
+    double rf = driveSubsystem.rightFrontDriveEncoder.getVelocity();
+    double rb = driveSubsystem.rightBackDriveEncoder.getVelocity();
+
+    if (! areTheyClose(lf, rf)) return false;
+    if (! areTheyClose(lf, rb)) return false;
+    if (! areTheyClose(lf, lb)) return false;
+    if (! areTheyClose(lb, rb)) return false;
+    if (! areTheyClose(lb, rf)) return false;
+    if (! areTheyClose(rb, rf)) return false;
+  return true;
+  }
 
   /**
    * Creates a new ExampleCommand.
@@ -22,6 +47,7 @@ public class RunWheelsForwardButton extends CommandBase {
     driveSubsystem = RobotContainer.driveSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveSubsystem);
+    SmartDashboard.putBoolean("diagnostic.drivewheels.ok", false);
   }
 
   // Called when the command is initially scheduled.
@@ -33,6 +59,9 @@ public class RunWheelsForwardButton extends CommandBase {
   public void execute() {
     driveSubsystem.testDrive(0, .42);
 
+    boolean ok = areAllwheelsok();
+    SmartDashboard.putBoolean("diagnostic.drivewheels.ok", ok);
+
     
   }
 
@@ -40,6 +69,7 @@ public class RunWheelsForwardButton extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     driveSubsystem.testDrive(0, 0);
+    SmartDashboard.putBoolean("diagnostic.drivewheels.ok", false);
 
   }
 
